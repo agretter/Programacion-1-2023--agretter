a9d3caf23a70661160fb4c2ad43fe0dc4524e8ff
agretter <alegretter.ag@gmail.com>
2023-04-18 19:14:39 -0300

    Pruebacomit

---
+++ inicio_23/inicio_23/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola, me das tu edad?");
+String laedadingresada = Console.ReadLine();
+int elañodelpibeenint = int.Parse(laedadingresada);
+int ladadelusuarioes = 2023 - elañodelpibeenint;
+Console.WriteLine("vos tenes " + ladadelusuarioes "años";
